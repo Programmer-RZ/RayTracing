1bbb0717875a3df66c3355ccdad44042709ab4fa
Ryan Zhang <ryan.zhang3@student.tdsb.on.ca>
2023-07-26 23:37:46 -0400

    Fixed premake5.lua in files param from .h to .hpp

--- RayTracing/premake5.lua
+++ RayTracing/premake5.lua
@@ -5,7 +5,7 @@ project "RayTracing"
    targetdir "bin/%{cfg.buildcfg}"
    staticruntime "off"
 
-   files { "src/**.h", "src/**.cpp" }
+   files { "src/**.hpp", "src/**.cpp" }
 
    includedirs
    {
